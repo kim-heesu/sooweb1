0ff9e98f621dc351d2248ceaf5eb8e4653763dec
kim-heesu <46507288+kim-heesu@users.noreply.github.com>
2021-03-22 15:14:15 +0900

    Add files via upload
    dd

---
+++ 20210322_Dearmesoon/js/layout.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    /*fullscroll*/
+    new fullScroll({
+        displayDots: true,
+        dotsPosition: 'Top',
+        animateTime: 0.7,
+        animateFunction: 'ease',
+    });
+});
+
