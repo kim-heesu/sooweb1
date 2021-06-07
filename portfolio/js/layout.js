d09a5e7f65d8df137b23cc91a5f07e0713cd23e4
kim-heesu <46507288+kim-heesu@users.noreply.github.com>
2021-06-07 16:54:07 +0900

    Add files via upload
    main

---
+++ portfolio/js/layout.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    /*docslider*/
+    docSlider.init({
+        dot:false
+    });
+
+   /* 타이핑 텍스트*/
+    var typingBool = false;
+    var typingIdx=0;
+    var liIndex = 0;
+    var liLength = $(".typing-txt>ul>li").length;
+    var del = -1;
+    var repeatInt= null;
+    var tyInt = null;
+
+    var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+
+    typingTxt=typingTxt.split("");
+
+    if(typingBool==false){
+        typingBool=true;
+        tyInt = setInterval(typing,150);
+    }
+    function typing(){
+        if(typingIdx<typingTxt.length){
+            $(".typing").append(typingTxt[typingIdx]);
+            typingIdx++;
+            if(typingIdx == typingTxt.length){
+                clearInterval(tyInt);
+                setTimeout(function(){
+                    tyInt = setInterval(typing,150);
+                },500);
+            }
+        } else{
+            if(-typingTxt.length-1 < del ){
+                $(".typing").html(typingTxt.slice(0, del))
+                del--;
+            }else{
+                if(liIndex >= liLength-1){
+                    liIndex=0;
+                }else{
+                    liIndex++;
+                }
+                typingIdx=0;
+                del= -1;
+                typingTxt = $(".typing-txt>ul>li").eq(liIndex).text();
+                clearInterval(tyInt);
+                setTimeout(function(){
+                    tyInt = setInterval(typing,130);
+                },500);
+            }
+        }
+    }
+    /* ///타이핑 텍스트*/
+
+});
+
